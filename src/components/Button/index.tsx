80abd469308338aefe37af97d0c4268bd6e351ef
Rayza Oliveira <rayza.ocr@gmail.com>
2021-03-25 16:39:38 -0300

    creating components of the project

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 
-const style: React.CSSProperties = {
-  background: "#7159c1",
-  color: "#fff",
+import { Container } from "./styles";
+export interface Props {
+  /**
+   * This is my favorite prop
+   */
+
+  backgroundColor?: string;
+  /**
+   * The color of button
+   */
+  color?: string;
+  outlined?: boolean;
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
+const ButtonWrapper: React.FC<Props> = ({
+  children,
+  backgroundColor = "#7159c1",
+  color = "#fff",
+  outlined = false,
+  ...props
+}) => {
+  return (
+    <Container
+      backgroundColor={backgroundColor}
+      color={color}
+      outlined={outlined}
+      {...props}
+    >
+      {children}
+    </Container>
+  );
 };
 
-export const Button: React.FC = ({ children }) => {
-  return <button>{children}</button>;
-};
+export { ButtonWrapper as Button };
